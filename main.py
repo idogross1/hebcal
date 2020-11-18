3ed449574e5b0c3611d24c7790d3c455f74a97e0
ido gross <idogross1@gmail.com>
2020-11-18 21:44:27 +0200

    updated

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import requests
+from Date import Date
 
-response = requests.get("https://www.hebcal.com/converter?cfg=json&hy=5749&hm=Kislev&hd=25&h2g=1")
-date = response.json()
+def main():
+    response = requests.get("https://www.hebcal.com/converter?cfg=json&hy=5749&hm=Kislev&hd=25&h2g=1")
+    date = response.json()
+    print(date['gy'])
+    
+    year = input("enter hebrew year:")
+    month = input("enter hebrew month:")
+    day = input("enter hebrew day:")
+    heb_date = Date(year, month, day)
+    
+
+
+if __name__ == '__main__':
+    main()
